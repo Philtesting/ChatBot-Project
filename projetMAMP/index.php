7226474651c4c0fb4cb88a09add5f559cac3580a
Lounis <lounis.bouayad@yahoo.com>
2020-03-06 19:11:37 +0100

    actualisation

--- projetMAMP/index.php
+++ projetMAMP/index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
 		<meta charset="utf-8">
-        <title>{% block title %}Welcome!{% endblock %}</title>
+        <title>ChatBot</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
         <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
@@ -15,68 +15,73 @@
         <div class="container">
             <div class="col-md-12 col-lg-6">
                 <div class="panel">
-                    <!--Heading-->
-                    <div class="panel-heading">
-                        <div class="panel-control">
-                            <div class="btn-group">
-                                <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#demo-chat-body"><i class="fa fa-chevron-down"></i></button>
-                            </div>
-                        </div>
-                        <h3 class="panel-title">Chat</h3>
-                    </div>
-            
+
                     <!--Widget body-->
-                    <div id="demo-chat-body" class="collapse in">
-                        <div class="nano has-scrollbar" style="height:380px">
+                    <div id="demo-chat-body" class="collapse">
+                        <div id = "scrolltop" class="nano has-scrollbar" style="height:380px">
                             <div class="nano-content pad-all" tabindex="0" style="right: -17px;">
                                 <ul id = "answerbox" class="list-unstyled media-block">
                                     <li class="mar-btm">
-    
-                                        <div class="media-body pad-hor">
-                                            <div class="speech">
-                                                <p>Bonjours je suis le chatbot, comment vous aidez?</p>
-                                                </p>
+                                        <div class="media-body pad-hor mar-btm">
+                                            <div style="margin-bottom: 10px;">
+                                                <div class="media-left">
+                                                    <img src="bot.jpeg" class="img-circle img-sm" alt="Profile Picture">
+                                                </div>
+                                                <div class="speech">
+                                                    <p>
+                                                    Bonjour 😊 Je suis votre assistant virtuel prêt à vous aider dans le cadre de vos activités financières ?<br>
+                                                    <br> 
+                                                    Je vous invite à choisir une des fonctionnalités suivantes 👇
+                                                    </p>
+                                                </div>
                                             </div>
+                                            <ul >
+                                                <li id="convertir" class="btn btn-primary" style="margin-bottom: 10px">
+                                                💸 Conversion d’argent 
+                                                </li>
+                                                <li class="btn btn-primary " style="margin-bottom: 10px;">
+                                                ❓Eligibilité au prêt bancaire
+                                                </li>
+                                                <li class="btn btn-primary "  style="margin-bottom: 10px;">
+                                                🏦 Quelle banque pour moi ? 
+                                                </li>
+                                            </ul>
                                         </div>
                                     </li>
-                                </ul>
+                                   </ul>
                             </div>
-                        <div class="nano-pane"><div class="nano-slider" style="height: 141px; transform: translate(0px, 0px);"></div></div></div>
+                            <div class="nano-pane">
+                                <div class="nano-slider" style="height: 141px; transform: translate(0px, 0px);">
+                                </div>
+                            </div>
+                        </div>
             
                         <!--Widget footer-->
                         <div class="panel-footer">
                             <div class="row">
-                                <form id="formtest" name = "questionValue">
-                                    <div class="col-xs-9">
-                                        <input id ="textbox" type="text" placeholder="Enter your text" class="form-control chat-input" name="question">
-                                    </div>
-                                    <div class="col-xs-3">
-                                        <button id="sendMessage" name="test" class="btn btn-primary btn-block" type="submit" >Send</button>
-                                    </div>
-                                </form>
+                                <div class="col-xs-9">
+                                    <input id ="textbox" onkeypress="pressEnter(event)" type="text" placeholder="Enter your text" class="form-control chat-input" name="question">
+                                </div>
+                                <div class="col-xs-3">
+                                    <button id="sendMessage" name="test" class="btn btn-primary btn-block" type="submit" >Send</button>
+                                </div>
                             </div>
                         </div>
+                    </div>
+                    <!--Heading-->
+                    <div class="panel-heading">
+                        <div class="panel-control">
+                            <div class="btn-group">
+                                <button class="btn btn-default" type="button" data-toggle="collapse" data-target="#demo-chat-body">
+                                    <i class="fa fa-chevron-down"></i>
+                                </button>
+                            </div>
+                        </div>
+                        <h3 class="panel-title">Chat</h3>
                     </div>
                 </div>
             </div>
         </div>
-        <?php
-        // define variables and set to empty values
-        
-        if (isset($_POST["test"])) {
-            $question = $_POST["question"];
-            $ok = "ok";
-            echo $question;
-          }
-        
-        
-        function test_input($data) {
-          $data = trim($data);
-          $data = stripslashes($data);
-          $data = htmlspecialchars($data);
-          return $data;
-        }
-        ?>
     </body>
     <script src="script.js"></script>
 </html>
--- projetMAMP/script.js
+++ projetMAMP/script.js
@@ -1,26 +1,51 @@
-/*
+
 var textbox = document.getElementById("textbox");
 var button = document.getElementById("sendMessage");
-*/
 var answerbox = document.getElementById("answerbox");
-var formQ = document.forms["questionValue"]["question"];
-var formId = document.getElementById('formtest');
 var d = new Date();
+var scroll = document.getElementById("scrolltop");
 
+var convertir = document.getElementById("convertir");
 
-formId.addEventListener("submit", function(){
-    cookieTime()
-    docformIdment.getElementById("myForm").submit();
-    createResponse()
+const CLIENT_TOKEN = 'ODFJVKZRIHQ63W7UHJYZFTQE3D3XXOOR'
+const uri = 'https://api.wit.ai/message?q=';
+const auth = 'Bearer ' + CLIENT_TOKEN;
+
+function pressEnter(evt) {
+    if(evt.keyCode == 13){
+        updateChat()
+    }
+}
+
+convertir.addEventListener("click", function(){
+    textbox.value="Convertir de la monnaie"
+    updateChat()
 });
-/*
+
 button.addEventListener("click", function(){
-    createResponse()
+    updateChat()
 });
-*/
-function createResponse(){
+
+function updateChat(){
+    var start = new Date().getTime();
+    console.log(start)
+
+    if(textbox.value.lenght === 0 || !textbox.value.trim()){
+        textbox.value = '';
+        return
+    }
+    clientQuestion(textbox.value)
+    sendToApi(textbox.value)
+    textbox.value = '';
+
+    var end = new Date().getTime();
+    console.log("cela a pris " + (end - start) + " milliseconds pour executer");
+} 
+
+function clientQuestion(msg){
+
     var marBtn = document.createElement("li");
-     marBtn.classList.add("mar-btn");
+     marBtn.classList.add("mar-btm");
      answerbox.appendChild(marBtn);
 
      var positionMessage = document.createElement("div");
@@ -35,7 +60,7 @@ function createResponse(){
 
      var newMessage = document.createElement("p");
      newMessage.setAttribute('id','question')
-     newMessage.innerHTML = document.cookie;
+     newMessage.innerHTML = msg;
      messageBox.appendChild(newMessage);
 
      var timeBox = document.createElement("p");
@@ -56,7 +81,131 @@ function createResponse(){
 }
 
 function cookieTime(){
+    /*
     d.setTime(d.getTime() + (60*60*1000));
     var expires = "expires="+ d.toUTCString();
     document.cookie = "data="+ formQ.value +";"+ expires;
+    */
+}
+
+function sendToApi(msg){
+    let q = encodeURIComponent(msg);
+    fetch(uri+q, {
+        headers: {
+            Authorization: auth
+        }
+    })
+    .then(res => res.json())
+    
+    .then(function(data) {
+        let entities = data.entities
+        if(Object.entries(entities).length === 0 && entities.constructor === Object){
+            chatBotResponse("Désoler je ne comprend pas ce que vous voulez dire par "+ data._text)
+        }
+        else{
+            if (entities.intent[0].confidence < 0.5){
+                chatBotResponse("Je ne me suis pas encore assez entrainer pour repondre a votre demande ")
+            }
+            else{
+                treatIntent(entities)
+            }
+        }
+    })
+    
+    .catch(err => {
+        console.log(err)
+        chatBotResponse("Je ne suis pas sûre d'avoir bien compris. Pourriez-vous préciser votre demande?")
+    });
+    
+}
+
+
+function treatIntent(entities){
+    if(entities.intent[0].value == "exchange_money"){
+        if (entities.hasOwnProperty('phrase_monnaie')){
+            chatBotResponse("Très bien donnez moi le montant de la somme d’argent que vous souhaitez convertir et en quelle devise ?")
+        }
+        else{
+            var before = entities.amount_before[0].entities.amount_exchange[0].value;
+            var after = entities.amount_exchange[0].value;
+            var amount = entities.amount_before[0].entities.amount_number[0].value;
+            amount.trim()
+            fetch("https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from="+before+"&to="+after+"&amount="+amount, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "currency-converter5.p.rapidapi.com",
+                    "x-rapidapi-key": "c3827751ecmshde9d57ab3e2ef66p13eaf6jsnbaddb15549a0"
+                }
+            })
+            .then(res => res.json())
+            .then(function(data) {
+                let exchange;
+                for (const [key, value] of Object.entries(data.rates)) {
+                    exchange = value.rate_for_amount
+                }
+                msg = amount+" "+before+" = "+exchange+" "+after
+                chatBotResponse(msg)
+            })
+            .catch(err => {
+                chatBotResponse("Indiquer moi la devise de entré et de sortis s'il vous plait")
+            });
+        }
+    }
+
+}
+
+
+function chatBotResponse(msg){
+    
+    var marBtn = document.createElement("li");
+    marBtn.classList.add("mar-btm");
+    answerbox.appendChild(marBtn);
+
+    var positionMessage = document.createElement("div");
+    positionMessage.classList.add("media-body");
+    positionMessage.classList.add("pad-hor");
+    marBtn.appendChild(positionMessage);
+
+    var catBox = document.createElement("div");
+    catBox.classList.add("media-left");
+    positionMessage.appendChild(catBox);
+
+    var imgBot = document.createElement("img");
+    imgBot.setAttribute('src','bot.jpeg')
+    imgBot.classList.add("img-circle");
+    imgBot.classList.add("img-sm");
+    imgBot.setAttribute('alt','Profile Picture')
+    catBox.appendChild(imgBot)
+
+    var messageBox = document.createElement("div");
+    messageBox.classList.add("speech");
+    positionMessage.appendChild(messageBox);
+
+    var newMessage = document.createElement("p");
+    newMessage.setAttribute('id','question')
+    let i = 0
+    var typing = setInterval(function(){ 
+        newMessage.innerHTML += msg.charAt(i);
+        i++;
+        if (i >= msg.length ) {
+            clearInterval(typing)
+        }
+    }, 60)
+    messageBox.appendChild(newMessage);
+
+    var timeBox = document.createElement("p");
+    timeBox.classList.add("speech-time");
+    messageBox.appendChild(timeBox);
+
+    var timeIcon = document.createElement("i");
+    timeIcon.classList.add("fa");
+    timeIcon.classList.add("fa-clock-o");
+    timeIcon.classList.add("fa-fw");
+    timeBox.appendChild(timeIcon);
+
+    var h = d.getHours();
+    var min = d.getMinutes();
+    var timeOfMessage = document.createTextNode(h+":"+min);
+    timeBox.appendChild(timeOfMessage);
+
 }
